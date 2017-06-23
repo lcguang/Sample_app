a99fc9602afcabc5cc8eaad585fb2e1620c50111
lcguang <guang12307130252@gmail.com>
2017-06-23 14:32:59 -0700

    Finish sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
+  resources :users
   get 'users/new', to: 'users#new', as: :signup
+  #post 'users/signup',  to: 'users#create', as: :signup
 
   get 'static_pages/home', to: 'static_pages#home', as: :home
-
   get 'static_pages/help', to: 'static_pages#help', as: :help
-
   get 'static_pages/about', to: 'static_pages#about', as: :about
-
   get 'static_pages/contact', to: 'static_pages#contact', as: :contact
 
   root 'static_pages#home'
