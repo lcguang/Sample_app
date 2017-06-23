01dc5c15ca47f1dceb787e5519c35291e3caf69c
lcguang <guang12307130252@gmail.com>
2017-06-22 19:47:00 -0700

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get 'static_pages/home'
+  get 'static_pages/home', to: 'static_pages#home', as: :home
 
-  get 'static_pages/help'
+  get 'static_pages/help', to: 'static_pages#help', as: :help
 
-  get 'static_pages/about'
+  get 'static_pages/about', to: 'static_pages#about', as: :about
 
-  get 'static_pages/contact'
+  get 'static_pages/contact', to: 'static_pages#contact', as: :contact
 
 end
